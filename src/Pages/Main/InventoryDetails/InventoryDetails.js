f22768ac2e4d8101ac7b0be4867fb6b811bc18ae
Shama <assiddik001@gmail.com>
2022-05-04 20:19:22 +0600

    create inventory hook

--- src/Pages/Main/InventoryDetails/InventoryDetails.js
+++ src/Pages/Main/InventoryDetails/InventoryDetails.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { useParams } from "react-router-dom";
+import useInventoryDetail from "../../../hooks/useInventoryDetail";
 
 const InventoryDetails = () => {
   const { id } = useParams();
+  const [inventory] = useInventoryDetail(id);
 
   return (
     <div>
-      <h2>Inventory details: {id}</h2>
+      <h2>Inventory details: {inventory._id}</h2>
     </div>
   );
 };
