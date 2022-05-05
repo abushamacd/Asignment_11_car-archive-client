4880cd97017bc1b965b204b99e13fb5e9056c224
Shama <assiddik001@gmail.com>
2022-05-05 07:15:30 +0600

    question 2

--- src/Pages/Main/Blogs/Blogs.js
+++ src/Pages/Main/Blogs/Blogs.js
@@ -12,7 +12,8 @@ const Blogs = () => {
           <span>Q1:</span> Difference between javascript and node js?
         </h2>
         <p>
-          <span>Ans:</span> <br /> <span className="primary">Javascript:</span>
+          <span>Ans:</span> <br />
+          <span className="primary">Javascript:</span>
           <br />
           1. Javascript is a scripting language that used in website.
           <br />
@@ -36,6 +37,31 @@ const Blogs = () => {
           <br />
           5. Modules like Lodash, Express, etc., are included in Nodejs.
           <br />
+        </p>
+      </div>
+      <div className="qa">
+        <h2>
+          <span>Q1:</span> Differences between sql and nosql databases?
+        </h2>
+        <p>
+          <span>Ans:</span>
+          <br /> 1. A SQL database is mainly referred to as a Relational
+          Database Management System (RDBMS); while a NoSQL database is
+          primarily referred to as a Non-Relational Database or a Distributed
+          Database.
+          <br />
+          2. SQL databases are generally vertically scalable.In contrast, NoSQL
+          databases are horizontally scalable.
+          <br />
+          3. A SQL database is structured around tables, whereas a NoSQL
+          database is structured around key value pairs database.
+          <br />
+          4. A SQL database follows the ACID principles (Atomicity, Consistency,
+          Isolation, Durability), while a NoSQL database follows Brewer's CAP
+          (Consistency, Availability, and Partition Tolerance).
+          <br />
+          5. SQL database such as - MySQL, PostgreSQL, Oracle, and Microsoft SQL
+          Server. NoSQL database such as - MongoDB, DynamoDB and CouchDB
         </p>
       </div>
     </Container>
